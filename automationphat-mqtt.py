8ce3e5939d3fe9a9cc28fb2091b743a263d6da84
sampod <sami.heino@gmail.com>
2019-06-19 17:34:57 +0000

    first commit

---
+++ automationphat-mqtt.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import configparser
+import time
+import paho.mqtt.client as mqtt
+
+import automationhat
+time.sleep(0.1) # short pause after ads1015 class creation recommended
+
+# initialize config
+config = configparser.ConfigParser()
+config.read("automationphat-mqtt.conf")
+# read variables from config
+sleeptime = config.getint('general', 'interval')
+mqttaddress = config.get("mqtt", "address")
+mqtttopic1 = config.get("mqtt", "automationphattopic1")
+mqtttopic2 = config.get("mqtt", "automationphattopic2")
+
+# Global variables
+global value1, value2
+
+# MQTT-sending
+# Calculate median for measured power values and send median and latest values
+# and counter value to MQTT broker
+def mqttsend():
+    global value1, value2
+    client =mqtt.Client("automationphat")
+    try:
+      client.connect(mqttaddress)
+      client.publish(mqtttopic1,value1)
+      print("mqtt value1 sent")
+      client.publish(mqtttopic2,value2)
+      print("mqtt value2 sent")
+      client.loop(2)
+      client.disconnect()
+      client.loop(2)
+    except:
+      print("MQTT connetion error")
+
+
+print("""
+Press CTRL+C to exit.
+""")
+
+while True:
+    value1 = automationhat.analog.one.read()
+    print(value1)
+    value2 = automationhat.analog.two.read()
+    print(value2)
+    mqttsend()
+    time.sleep(sleeptime)
+
