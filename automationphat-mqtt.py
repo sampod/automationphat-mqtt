688ce3c9fd049dd2929e8fbaeb0aea06bd3299ce
sampod <sami.heino@gmail.com>
2021-09-23 16:01:00 +0100

    Added automation hat features. Changed topics.

--- automationphat-mqtt.py
+++ automationphat-mqtt.py
@@ -4,12 +4,18 @@ import schedule
 import sys
 import configparser
 import time
+import signal
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
+import numbers
 from pathlib import PurePath
 
 import automationhat
 time.sleep(0.1) # short pause after ads1015 class creation recommended
+
+if automationhat.is_automation_hat():
+    automationhat.light.power.write(1)
+    automationhat.enable_auto_lights(True)
 
 # initialize config
 config = configparser.ConfigParser()
@@ -47,12 +53,25 @@ def on_connect(client, userdata, flags, rc):
     if rc==0:
         client.connected_flag=True
         client.subscribe(relay_ctltopic)
+        client.subscribe(relay_ctltopic2)
+        client.subscribe(relay2_ctltopic1)
+        client.subscribe(relay2_ctltopic2)
+        client.subscribe(relay3_ctltopic1)
+        client.subscribe(relay3_ctltopic2)
         client.subscribe(out1_ctltopic)
         client.subscribe(out2_ctltopic)
         client.subscribe(out3_ctltopic)
+        client.subscribe(out1_ctltopic2)
+        client.subscribe(out2_ctltopic2)
+        client.subscribe(out3_ctltopic2)
+        if automationhat.is_automation_hat():
+            automationhat.light.comms.on()
+
     else:
         client.bad_connection_flag=True
         client.connected_flag=False
+        if automationhat.is_automation_hat():
+            automationhat.light.comms.off()
 
 # Send digital input pulses instantly to MQTT broker
 def pulsecallback(channel):
@@ -85,10 +104,18 @@ GPIO.add_event_detect(20, GPIO.BOTH, callback=pulsecallback)
 GPIO.add_event_detect(21, GPIO.BOTH, callback=pulsecallback)
 ##MQTT initialisations
 messages=[]
-relay_ctltopic=mqtttopic + "16/control/#"
+relay_ctltopic=mqtttopic + "13/control/#"
+relay_ctltopic2=mqtttopic + "relay1/control/#"
+relay2_ctltopic1=mqtttopic + "19/control/#"
+relay2_ctltopic2=mqtttopic + "relay2/control/#"
+relay3_ctltopic1=mqtttopic + "16/control/#"
+relay3_ctltopic2=mqtttopic + "relay3/control/#"
 out1_ctltopic=mqtttopic + "5/control/#"
 out2_ctltopic=mqtttopic + "12/control/#"
 out3_ctltopic=mqtttopic + "6/control/#"
+out1_ctltopic2=mqtttopic + "output1/control/#"
+out2_ctltopic2=mqtttopic + "output2/control/#"
+out3_ctltopic2=mqtttopic + "output3/control/#"
 client= mqtt.Client("GPIO-client-001")  #create client object client1.on_publish = on_publis
 client.on_message=on_message
 client.on_connect=on_connect
@@ -104,14 +131,18 @@ Press CTRL+C to exit.
 client.loop_start()
 while True:
     try:
-        relaytopic=mqtttopic + "16"
-        out1topic=mqtttopic + "5"
-        out2topic=mqtttopic + "12"
-        out3topic=mqtttopic + "6"
-        client.publish(relaytopic,GPIO.input(16))
-        client.publish(out1topic,GPIO.input(5))
-        client.publish(out2topic,GPIO.input(12))
-        client.publish(out3topic,GPIO.input(6))
+        relay1topic=mqtttopic + "relay1"
+        relay2topic=mqtttopic + "relay2"
+        relay3topic=mqtttopic + "relay3"
+        out1topic=mqtttopic + "output1"
+        out2topic=mqtttopic + "output2"
+        out3topic=mqtttopic + "output3"
+        client.publish(relay1topic, automationhat.relay.one.read())
+        client.publish(relay2topic, automationhat.relay.two.read())
+        client.publish(relay3topic, automationhat.relay.three.read())
+        client.publish(out1topic, automationhat.output.one.read())
+        client.publish(out2topic, automationhat.output.two.read())
+        client.publish(out3topic, automationhat.output.three.read())
         time.sleep(1)
 # process messages if received
         if len(messages)>0:
@@ -119,8 +150,26 @@ while True:
             msgpath = PurePath(m[0])
             relpath = msgpath.relative_to(mqtttopic)
             print("received ",m)
-            print("Controlling GPIO:",relpath.parts[0]," to:",int(m[1]))
-            GPIO.output(int(relpath.parts[0]),int(m[1])) #set
+            if m[1] == "0" or m[1]=="1":
+                print("Controlling GPIO:",relpath.parts[0]," to:",int(m[1]))
+                if relpath.parts[0] == "relay1":
+                    automationhat.relay.one.write(int(m[1]))
+                elif relpath.parts[0] == "relay2":
+                    if automationhat.is_automation_hat():
+                        automationhat.relay.two.write(int(m[1]))
+                elif relpath.parts[0] == "relay3":
+                    if automationhat.is_automation_hat():
+                        automationhat.relay.three.write(int(m[1]))
+                elif relpath.parts[0] == "output1":
+                    automationhat.output.one.write(int(m[1]))
+                elif relpath.parts[0] == "output2":
+                    automationhat.output.two.write(int(m[1]))
+                elif relpath.parts[0] == "output3":
+                    automationhat.output.three.write(int(m[1]))
+                else:
+                    GPIO.output(int(relpath.parts[0]),int(m[1])) #set
+            else:
+                print("Invalid control string - try 0 or 1")
 # keep scheduled processes running
         schedule.run_pending()
     except KeyboardInterrupt:
